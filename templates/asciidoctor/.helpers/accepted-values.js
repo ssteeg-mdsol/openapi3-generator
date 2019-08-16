b56caf42e2aec75a10998f2a3b9c03f4b78d9a2b
Scott Steeg <ssteeg@mdsol.com>
2019-08-16 13:54:13 +0900

    Update templates to improve the look of the generated PDF

--- templates/asciidoctor/.helpers/accepted-values.js
+++ templates/asciidoctor/.helpers/accepted-values.js
@@ -3,7 +3,7 @@ module.exports = (Handlebars, _) =>{
   Handlebars.registerHelper('acceptedValues', items =>{
     if(!items) return 'Any';
 
-    return items.map(i => `<code>${i}</code>`).join(', ');
+    return items.map(i => `\`${i}\``).join(', ');
   });
 
 }
